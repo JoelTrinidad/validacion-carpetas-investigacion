43debf733f9fda201bacde00d766e7d8e242b65d
Joel <joel@MacBook-Pro-de-Joel.local>
2022-04-15 14:18:14 -0500

    first commit

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id('uuid');
+            $table->string('dependencia');
+            $table->string('curp')->unique();
+            $table->string('email')->unique();
+            $table->string('oficio_alta', 60);
+            $table->string('oficio_baja', 60)->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamp('deleted_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
