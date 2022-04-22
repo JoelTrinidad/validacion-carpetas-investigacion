38b553c0e955f6c24f3a6c8c779c2005c53b3db6
Joel <joel@mbp-de-joel.lan>
2022-04-22 15:08:43 -0500

    popup and number of columns validation in importer file

--- app/Http/Controllers/CarpetasInvestigacionController.php
+++ app/Http/Controllers/CarpetasInvestigacionController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 /* use Illuminate\Support\Facades\Validator; */
 use Validator;
+use Importer;
 
 class CarpetasInvestigacionController extends Controller
 {
@@ -22,16 +23,32 @@ class CarpetasInvestigacionController extends Controller
             'file' => ['required']
         ]);
         if ($validator->passes()) {
-
+            #guardo archivo
             $dataTime = date('Ymd_His');
             $file = $request -> file('file');
             $fileName = $dataTime . '-' . $file -> getClientOriginalName();
             $savePath = public_path('/upload/');
-            
             $file -> move($savePath, $fileName);
-
-            return redirect()->back()
-                    ->with(['success'=>'File uploaded successfuly.']);
+            #leyendo archivo
+            $excel = Importer::make('Excel');
+            $excel -> load($savePath . $fileName);
+            $collection = $excel->getCollection();
+            #validacion de num de columnas
+            if (sizeof($collection[1]) == 1) {
+                $numRows = sizeof($collection);
+                for ($row=1; $row < $numRows; $row++) { 
+                    try {
+                        var_dump($collection[$row]);
+                    } catch (\Exception $e) {
+                        return redirect()->back()
+                                ->with(['errors'=>$e -> getMessage()]);
+                    }
+                }
+            } else {
+                return redirect()->back()
+                        ->with(['errors'=>[0 => 'Please provide data in file no more than 1 colum.']]);
+            }
+            
         } else {
             return redirect()->back()
                     ->with(['errors'=>$validator->errors()->all()]);
--- resources/views/excel.blade.php
+++ resources/views/excel.blade.php
@@ -1,28 +1,55 @@
-
 @extends('adminlte::page')
 @section('content')
-<div class="col-6 mx-auto pt-4">
+    <div class="col-6 mx-auto pt-4">
 
-    <div class="card card-primary ">
-        <div class="card-body">
-            <form action="{{ route('import') }}" method="POST" enctype="multipart/form-data">
-                @csrf
-                @if (session('errors'))
-                    @foreach ($errors as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                    <br><br>
-                @endif
-                @if (session('success'))
-                    {{ session('success') }}
-                <br><br>
-                @endif
-                
-                Select excel file to upload <br><br>
-                <input type="file" class="form-control" name="file" id="file"> <br><br>
-                <button type="submit" class="btn btn-block btn-primary ">Upload File</button>
-            </form>
+        <div class="card card-primary ">
+            <div class="card-body">
+                <form action="{{ route('import') }}" method="POST" enctype="multipart/form-data" id="excelForm">
+                    @csrf
+                    @if (session('errors'))
+                        @foreach ($errors as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                        <br><br>
+                    @endif
+                    @if (session('success'))
+                        {{ session('success') }}
+                        <br><br>
+                    @endif
+
+                    Select excel file to upload <br><br>
+                    <input type="file" class="form-control" name="file" id="file"> <br><br>
+                    <button type="button" class="btn btn-block btn-primary " data-toggle="modal" data-backdrop="static"
+                        data-keyboard="false" data-target="#myModalHorizontal">Subir archivo</button>
+                </form>
+            </div>
         </div>
     </div>
-</div>
+
+    <!-- Modal -->
+    <div class="modal fade" id="myModalHorizontal" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <!-- Modal Header -->
+                <div class="modal-header justify-content-center">
+                    <h4 class="modal-title" id="myModalLabel">Validación de Carpetas de Investigación</h4>
+                </div> <!-- Modal Body -->
+                <div class="bg-info text-center">
+                    Aviso
+                </div>
+                <div class="modal-body">
+                    <div class="text-center">
+                        <p>El tiempo de procesamiento y búsqueda puede ser demasiado largo</p>
+                        <p>¿Desea continuar?</p>
+                    </div>
+                    <div class="modal-body">
+                        <div class="modal-footer justify-content-center">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">No</button>
+                            <button type="submit" form="excelForm" class="btn btn-primary">Si</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @stop
