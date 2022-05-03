e0e075a262e5594bdd3a95aa2efde1472bdfc58b
Joel <joel@MacBook-Pro-de-Joel.local>
2022-05-03 17:05:40 -0500

    main route changed

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
 
 Route::get('/import','CarpetasInvestigacionController@importFile')->name('import');
 Route::post('/import','CarpetasInvestigacionController@importExcel');
