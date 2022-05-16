ca52d7269955a1e11636b120d9708de1a3034303
Joel <joel@MacBook-Pro-de-Joel.local>
2022-05-16 18:45:07 -0500

    UserFactory updated

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,9 +19,10 @@ use Illuminate\Support\Str;
 
 $factory->define(User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
+        'dependencia' => $faker->randomElement(['c5', 'ssc']),
+        'curp' => strtoupper($faker->unique()->bothify('????######??????##')),
         'email' => $faker->unique()->safeEmail,
-        'email_verified_at' => now(),
+        'oficio_alta' => $faker->realText(100),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
     ];
